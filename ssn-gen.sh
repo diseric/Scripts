29739a976d105a1a3e1bd9ddd7649f484819822d
Eric <ericmkelly@gmail.com>
2022-11-03 09:51:04 -0700

    Update ssn-gen.sh

--- ssn-gen.sh
+++ ssn-gen.sh
@@ -19,34 +19,34 @@ function validate() {
   if [ ${#SSN} -ne 9 ]
   then
     valid=0
-    echo "Invalid: SSN length is not 9 characters"
+ #   echo "Invalid: SSN length is not 9 characters"
   fi
 
   # No group can be all zeros
   if [ ${SSN:0:3} -eq "000" ] || [ ${SSN:3:2} -eq "00" ] || [ ${SSN:5:4} -eq "0000" ]
   then
     valid=0
-    echo "Invalid: No SSN group may be comprised of all zeroes"
+  #  echo "Invalid: No SSN group may be comprised of all zeroes"
   fi
 
   # Cannot start with '9'
   if [ ${SSN:0:1} -eq "9" ]
   then
     valid=0
-    echo "Invalid: SSN Cannot begin with a 9"
+  #  echo "Invalid: SSN Cannot begin with a 9"
   fi
 
   # First group cannot be comprised of only 6
   if [ ${SSN:0:3} -eq "666" ]
   then
     valid=0
-    echo "Invalid: Primary SSN group cannot be 666"
+ #   echo "Invalid: Primary SSN group cannot be 666"
   fi
 
   if [ ${SSN} -lt "987654329" ] && [ ${SSN} -gt "987654320" ]
   then
     valid=0
-    echo "Invalid: Number range between 987654329 and 987654320 is not allowed"
+ #   echo "Invalid: Number range between 987654329 and 987654320 is not allowed"
   fi
 
 }
@@ -56,8 +56,8 @@ valid=0
 while [ $valid -eq 0 ]
 do
   generate
-  echo -e "\nTesting generated number ${SSN} for validity..."
+#  echo -e "\nTesting generated number ${SSN} for validity..."
   validate
 done
 
-echo -e "${SSN:0:3}-${SSN:3:2}-${SSN:5:4}\n"
+echo -e "${SSN:0:3}-${SSN:3:2}-${SSN:5:4}"
